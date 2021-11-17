7111b8db4c0778f2f5a041b4d132ddabf95dbe4c
namChul <knc219@naver.com>
2021-11-17 10:45:47 +0900

    add spring security config

--- src/main/java/knc/assignment/kncshop/SecurityConfig.java
+++ src/main/java/knc/assignment/kncshop/SecurityConfig.java
@@ -1,4 +1,4 @@
-package knc.assignment.security;
+package knc.assignment.kncshop;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -6,20 +6,18 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
-public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
-
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring()
-                .antMatchers("/css/**","/js/**","/img/**","/lib/**");
-    }
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/**").permitAll();
+                .antMatchers("/resources/**");
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("/**").permitAll();
+    }
 }
