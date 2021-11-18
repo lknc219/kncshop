c89b9719444ff1edae1de8344b106a2a7400b1fd
lknc219 <knc219@naver.com>
2021-11-18 17:59:09 +0900

    자바스크립트로 include 적용, header.html 작업중

--- src/test/java/knc/assignment/kncshop/service/MemberServiceTest.java
+++ src/test/java/knc/assignment/kncshop/service/MemberServiceTest.java
@@ -2,6 +2,7 @@ package knc.assignment.kncshop.service;
 
 import knc.assignment.kncshop.domain.Member;
 import knc.assignment.kncshop.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,6 +10,8 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -26,14 +29,17 @@ class MemberServiceTest {
     @Autowired
     EntityManager em;
 
-    public void 회원가입() {
+    @Test
+    public void 회원가입() throws Exception {
         Member member = new Member();
         member.setName("김남철");
 
         Long joinId = memberService.join(member);
         em.flush();
 
-        assertEquals(member, memberRepository.findById(joinId));
+
+        Member findMember = memberRepository.findById(joinId).orElse(new Member());
+        assertEquals(member, findMember);
 
     }
 
