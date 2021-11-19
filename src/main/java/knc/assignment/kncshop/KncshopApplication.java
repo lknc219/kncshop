60bbea4b4b40cce4b2116b64c23151fa603e6dd5
lknc219 <knc219@naver.com>
2021-11-19 18:03:39 +0900

    java 1.8, mariadb 10 으로 변경중

--- src/main/java/knc/assignment/kncshop/KncshopApplication.java
+++ src/main/java/knc/assignment/kncshop/KncshopApplication.java
@@ -1,11 +1,9 @@
 package knc.assignment.kncshop;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@EnableAutoConfiguration(exclude = {SecurityException.class})
 public class KncshopApplication {
 
 	public static void main(String[] args) {
--- src/main/java/knc/assignment/kncshop/controller/MemberController.java
+++ src/main/java/knc/assignment/kncshop/controller/MemberController.java
@@ -1,2 +1,23 @@
-package knc.assignment.kncshop.controller;public class MemberController {
+package knc.assignment.kncshop.controller;
+
+import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@Log4j2
+public class MemberController {
+    
+    @GetMapping("/member/join")
+    public String MemberJoinView() {
+        return "/member/memberJoin";
+    }
+
+    @PostMapping("/member/join")
+    public String MemberJoin() {
+        log.info("확인사항ㅇㅇㅇ");  
+        return "/index";
+    }
 }
