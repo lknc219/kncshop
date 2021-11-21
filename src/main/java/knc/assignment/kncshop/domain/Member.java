bc7a5309a248dac9acb45b079dfb61f5338c9f48
namChulHome <knc219@naver.com>
2021-11-22 00:00:54 +0900

    member id 전략 수정

--- src/main/java/knc/assignment/kncshop/domain/Member.java
+++ src/main/java/knc/assignment/kncshop/domain/Member.java
@@ -3,10 +3,7 @@ package knc.assignment.kncshop.domain;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Embedded;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
@@ -14,7 +11,7 @@ import java.time.LocalDateTime;
 @Setter
 public class Member {
 
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String username;
