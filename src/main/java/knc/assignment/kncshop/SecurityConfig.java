b0e92884fdcf53a2c0a412f02ac425b9244254e3
namChulHome <knc219@naver.com>
2021-11-24 00:03:24 +0900

    권한에 따른 RestApi 구현하기

--- src/main/java/knc/assignment/kncshop/SecurityConfig.java
+++ src/main/java/knc/assignment/kncshop/SecurityConfig.java
@@ -1,10 +1,12 @@
 package knc.assignment.kncshop;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
---
+++ src/main/java/knc/assignment/kncshop/controller/MemberApiController.java
@@ -0,0 +1,9 @@
+package knc.assignment.kncshop.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MemberApiController {
+
+
+}
